d7c529e778d9aaa684ed2e32f94ff8aca1265553
Dan Reuman <reuman@ku.edu>
2019-02-22 12:42:41 -0600

    changed order of panels in pedagog fig

--- fig_example_timeseries.R
+++ fig_example_timeseries.R
@@ -26,33 +26,33 @@ tiff("Figs/fig_example_timeseries.tif",
      width=3.5, height=7, units="in",res=600,compression = "lzw")
 op<-par(mfrow=c(3,1),oma=c(1,0.5,0,0), mar=c(3,3.5,1,1),mgp=c(2.2,0.5,0), cex.lab=1.2)
 
-#plot the synchronous component
-pl<-20
-plot(t[1:pl],s1[1:pl],type='l',
-     xlab=NA,ylab="pop. component",
-     ylim=c(min(s1,s2),max(s1,s2)))
-mtext("(a)",3,cex=0.8,adj=0.02,line=-1.2)
-
-#plot the asynchronous components
-plot(t[1:pl],s2[1:pl,1],type='l',col=rainbow(length(phi))[1],
-     xlab=NA,ylab="pop. component",
-     ylim=c(min(s1,s2),max(s1,s2)+0.1))
-for (counter in 2:length(phi))
-{
-  lines(t[1:pl],s2[1:pl,counter],type='l',col=rainbow(length(phi))[counter])
-}
-#lines(t[1:pl],apply(FUN=mean,X=s2[1:pl,],MAR=1),type='l',col='black',cex=2)
-mtext("(b)",3,cex=0.8,adj=0.02,line=-1.2)
-
 #plot the aggregate time series
+pl<-20
 plot(t[1:pl],s.all[1:pl,1],type='l',col=rainbow(length(phi))[1],
-     xlab="time",ylab="population",
+     ylab="population",xlab=NA,
      ylim=c(min(s.all),max(s.all)+0.1))
 for (counter in 2:length(phi))
 {
   lines(t[1:pl],s.all[1:pl,counter],type='l',col=rainbow(length(phi))[counter])
 }
 lines(t[1:pl],apply(FUN=mean,X=s.all[1:pl,],MAR=1),type='l',col='black',lwd=2)
+mtext("(a)",3,cex=0.8,adj=0.02,line=-1.2)
+
+#plot the synchronous component
+plot(t[1:pl],s1[1:pl],type='l',
+     xlab=NA,ylab="pop. component",
+     ylim=c(min(s1,s2),max(s1,s2)))
+mtext("(b)",3,cex=0.8,adj=0.02,line=-1.2)
+
+#plot the asynchronous components
+plot(t[1:pl],s2[1:pl,1],type='l',col=rainbow(length(phi))[1],
+     xlab="time",ylab="pop. component",
+     ylim=c(min(s1,s2),max(s1,s2)+0.1))
+for (counter in 2:length(phi))
+{
+  lines(t[1:pl],s2[1:pl,counter],type='l',col=rainbow(length(phi))[counter])
+}
+#lines(t[1:pl],apply(FUN=mean,X=s2[1:pl,],MAR=1),type='l',col='black',cex=2)
 mtext("(c)",3,cex=0.8,adj=0.02,line=-1.2)
 
 par(op)
